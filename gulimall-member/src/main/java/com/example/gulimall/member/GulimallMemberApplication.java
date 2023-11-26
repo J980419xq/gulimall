c72763960f2e82b89f029b40dcf051773555d266
J980419xq <1908393204@qq.com>
2023-11-26 20:08:22 +0800

    open-feign

--- gulimall-member/src/main/java/com/example/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/example/gulimall/member/GulimallMemberApplication.java
@@ -3,9 +3,11 @@ package com.example.gulimall.member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.example.gulimall.member.feign")
 public class GulimallMemberApplication {
 
     public static void main(String[] args) {
